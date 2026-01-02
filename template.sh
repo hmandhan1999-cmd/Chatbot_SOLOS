82b9da43fe8df4837116658b3ee61eb4b8cde26b
hitesh <hitesh@sellogs.com>
2026-01-02 13:45:59 +0530

    new_msg

---
+++ template.sh
@@ -0,0 +1,24 @@
+#Create a new project
+mkdir -p src
+mkdir -p research
+
+#Create a new file
+touch src/index.py
+touch .env
+
+#Create a new requirements.txt
+touch requirements.txt
+
+#Create a new Dockerfile
+touch Dockerfile
+
+#Create a Readme.md
+touch README.md
+
+#Create a Jupyter notebook
+touch research/notebook.ipynb
+
+
+
+
+
